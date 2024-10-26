22d4b3ca0d685c56954143d240fb2e4f65a32ea1
sixpennyfox4 <asenoprenov2022@gmail.com>
2024-10-26 15:21:58 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,6 +1,6 @@
 local module = {}
 
 module.LocalPLR = game.Players.LocalPlayer
-module.CurrentWalkSpeed = module.LocalPLR.Character.Humanoid.WalkSpeed
+module.CurrentWalkSpeed = game.Players.LocalPlayer.Character.Humanoid.WalkSpeed
 
 return module
